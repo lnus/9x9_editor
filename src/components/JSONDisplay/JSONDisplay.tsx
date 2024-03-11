09e2b7347a0220a19faa8ef974478608ca193994
Linus Wæhler <linus.wahler@gmail.com>
2024-03-11 12:03:31 +0100

    Working mod sorting

--- src/components/JSONDisplay/JSONDisplay.tsx
+++ src/components/JSONDisplay/JSONDisplay.tsx
@@ -1,7 +1,7 @@
 import { SchematicJSON, RootEntry } from '@/interfaces/SchematicJSON';
 import { Text, Image, Badge, Button, Card, Group, Grid, TextInput } from '@mantine/core';
 import { modals } from '@mantine/modals';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Autocomplete } from '@mantine/core';
 import { MinecraftItems } from '@/data/MinecraftItems';
 
@@ -52,29 +52,24 @@ function ItemDisplay({
   const icon = 'https://via.placeholder.com/150';
 
   return (
-    <Grid.Col span={4} key={name}>
-      <Card style={{ padding: '1em' }}>
-        <Card.Section>
-          <Image src={icon} height={150} alt={name} />
-        </Card.Section>
-        <Group justify="center" gap="sm">
-          <Text>{name}</Text>
-          <Badge>{count}</Badge>
-        </Group>
-        {/* Open a modal to change the name */}
-        <Button
-          onClick={() => {
-            modals.open({
-              centered: true,
-              title: 'Change item type',
-              children: <ModalContent name={name} updateItem={updateItem} />,
-            });
-          }}
-        >
-          Change Name
-        </Button>
-      </Card>
-    </Grid.Col>
+    <Card style={{ padding: '1em' }}>
+      <Group justify="center" gap="sm">
+        <Text>{name}</Text>
+        <Badge>{count}</Badge>
+      </Group>
+      {/* Open a modal to change the name */}
+      <Button
+        onClick={() => {
+          modals.open({
+            centered: true,
+            title: 'Change item type',
+            children: <ModalContent name={name} updateItem={updateItem} />,
+          });
+        }}
+      >
+        Change Name
+      </Button>
+    </Card>
   );
 }
 
@@ -88,6 +83,19 @@ export function MaterialList({
   updateItem: (oldItem: string, newItem: string) => void;
 }) {
   const [searchQuery, setSearchQuery] = useState('');
+  const [items, setItems] = useState<RootEntry[]>(jsonData.header.material_list.root_entry);
+  const [mods, setMods] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newItems = jsonData.header.material_list.root_entry.filter((item) => {
+      return item.item.id.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+    setItems(newItems); // This is just visual, the original data is not changed
+
+    // Get all the mods
+    const mods = newItems.map((item) => item.item.id.split(':')[0]);
+    setMods(Array.from(new Set(mods)));
+  }, [jsonData, searchQuery]);
 
   return (
     <div>
@@ -98,15 +106,29 @@ export function MaterialList({
         m="lg"
         onChange={(event) => setSearchQuery(event.currentTarget.value)}
       />
-      <Grid m="lg">
-        {jsonData.header.material_list.root_entry
-          .filter((item) => {
-            return item.item.id.toLowerCase().includes(searchQuery.toLowerCase());
-          })
-          .map((item, index) => (
-            <ItemDisplay key={index} item={item} updateItem={updateItem} />
+      {mods.map((mod, index) => (
+        <div>
+          <Text key={index} size="xl">
+            {mod.slice(0, 1).toUpperCase() + mod.slice(1)}
+          </Text>
+
+          {/* TODO: This is really inefficient, we should use a better way to group the items
+
+          Iterate over the items which have the same mod
+          This is a bit more complex, but it's doable
+
+          For some reason this is the only way to force re-rendering when
+          items change groups 
+          */}
+          {items.map((item, index) => (
+            <div key={index}>
+              {item.item.id.split(':')[0] === mod && (
+                <ItemDisplay key={item.item.id} item={item} updateItem={updateItem} />
+              )}
+            </div>
           ))}
-      </Grid>
+        </div>
+      ))}
     </div>
   );
 }
